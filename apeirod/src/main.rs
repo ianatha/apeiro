9b741598d6f8f3984bc74f080ddc8eacb4fdb8be
Ian Atha <ian@atha.io>
2023-01-29 14:25:49 +0200

    fdb wip

--- apeirod/src/main.rs
+++ apeirod/src/main.rs
@@ -5,6 +5,9 @@ use actix_web::{App, HttpServer};
 use apeiro_engine::plugins::PluginConfiguration;
 use apeiro_engine::{get_engine_runtime, DEngine};
 use clap::{command, Parser};
+use fdb::database::FdbDatabase;
+use fdb::subspace::Subspace;
+use fdb::tuple::Tuple;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use tracing::Level;
@@ -54,8 +57,138 @@ pub fn establish_db_connection(
     Ok(pool)
 }
 
+use fdb::range::{Range, RangeOptions};
+use fdb::transaction::{ReadTransaction, Transaction};
+use fdb::KeySelector;
+
+async fn foundationdb_test() -> Result<(), Box<dyn std::error::Error>> {
+    use bytes::Bytes;
+
+    use tokio::runtime::Runtime;
+    use tokio_stream::StreamExt;
+
+    use std::env;
+    use std::error::Error;
+
+    let fdb_cluster_file = env::var("FDB_CLUSTER_FILE")
+        .unwrap_or_else(|_| "/usr/local/etc/foundationdb/fdb.cluster".to_string());
+    // .expect("FDB_CLUSTER_FILE not defined!");
+
+    unsafe {
+        fdb::select_api_version(fdb::FDB_API_VERSION as i32);
+        fdb::start_network();
+    }
+
+    let fdb_database = fdb::open_database(fdb_cluster_file)?;
+
+    // // Clear the database.
+    // fdb_database
+    //     .run(|tr| async move {
+    //         tr.clear_range(Range::new(Bytes::new(), Bytes::from_static(b"\xFF")));
+
+    //         Ok(())
+    //     })
+    //     .await?;
+
+    // Set a few key values.
+    fdb_database
+        .run(|tr| async move {
+            tr.set(Bytes::from("apple"), Bytes::from("foo"));
+            tr.set(Bytes::from("cherry"), Bytes::from("baz"));
+            tr.set(Bytes::from("banana"), Bytes::from("bar"));
+
+            Ok(())
+        })
+        .await?;
+
+    println!("non-snapshot range read");
+
+    fdb_database
+        .run(|tr| async move {
+            let mut range_stream = tr.get_range(
+                KeySelector::first_greater_or_equal(Bytes::new()),
+                KeySelector::first_greater_or_equal(Bytes::from_static(b"\xFF")),
+                RangeOptions::default(),
+            );
+
+            while let Some(x) = range_stream.next().await {
+                let (key, value) = x?.into_parts();
+                println!(
+                    "{} is {}",
+                    String::from_utf8_lossy(&Bytes::from(key)[..]),
+                    String::from_utf8_lossy(&Bytes::from(value)[..])
+                );
+            }
+
+            println!();
+
+            let mut range_stream = Range::new(Bytes::new(), Bytes::from_static(b"\xFF"))
+                .into_stream(&tr, RangeOptions::default());
+
+            while let Some(x) = range_stream.next().await {
+                let (key, value) = x?.into_parts();
+                println!(
+                    "{} is {}",
+                    String::from_utf8_lossy(&Bytes::from(key)[..]),
+                    String::from_utf8_lossy(&Bytes::from(value)[..])
+                );
+            }
+
+            Ok(())
+        })
+        .await?;
+
+    println!();
+    println!("snapshot range read");
+
+    fdb_database
+        .read(|tr| async move {
+            let mut range_stream = tr.get_range(
+                KeySelector::first_greater_or_equal(Bytes::new()),
+                KeySelector::first_greater_or_equal(Bytes::from_static(b"\xFF")),
+                RangeOptions::default(),
+            );
+
+            while let Some(x) = range_stream.next().await {
+                let (key, value) = x?.into_parts();
+                println!(
+                    "{} is {}",
+                    String::from_utf8_lossy(&Bytes::from(key)[..]),
+                    String::from_utf8_lossy(&Bytes::from(value)[..])
+                );
+            }
+
+            println!();
+
+            let mut range_stream = Range::new(Bytes::new(), Bytes::from_static(b"\xFF"))
+                .into_stream(&tr, RangeOptions::default());
+
+            while let Some(x) = range_stream.next().await {
+                let (key, value) = x?.into_parts();
+                println!(
+                    "{} is {}",
+                    String::from_utf8_lossy(&Bytes::from(key)[..]),
+                    String::from_utf8_lossy(&Bytes::from(value)[..])
+                );
+            }
+
+            Ok(())
+        })
+        .await?;
+
+    drop(fdb_database);
+
+    unsafe {
+        fdb::stop_network();
+    }
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    foundationdb_test().await.unwrap();
+    panic!();
     // console_subscriber::ConsoleLayer::builder()
     //     .retention(Duration::from_secs(120))
     //     .init();
