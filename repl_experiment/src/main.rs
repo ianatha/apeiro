5eceff9be60113996251106dd09257868dd4adbe
Ian Atha <ian@atha.io>
2023-03-01 22:04:52 +0200

    repl experiment

--- repl_experiment/src/main.rs
+++ repl_experiment/src/main.rs
@@ -31,14 +31,24 @@ async fn main() -> anyhow::Result<()> {
 
     let v8_key_scope = v8::String::new(scope, "$scope").unwrap();
 
+    let stdin = io::stdin();
+    print!("> ");
+    io::stdout().flush()?;
+
+    let mut persist_apeiro_scope: Option<serde_json::Value> = None;
+
+    // read file scope.json into a string called x
+    if let Result::Ok(stored_scope) = std::fs::read_to_string("scope.json") {
+        let x: serde_json::Value = serde_json::from_str(&stored_scope).unwrap();
+        let scope_in_v8 = serde_v8::ramson_to_v8(scope, &x).unwrap();
+        persist_apeiro_scope = Some(x);
+        context.global(scope).set(scope, v8_key_scope.into(), scope_in_v8).unwrap();
+    }
+
     let mut init_script = apeiro_compiler::apeiro_compile("function empty() {}".into()).unwrap().compiled_src;
     init_script.push_str("\n globalThis.$scope = $scope");
     println!("{}", init_script);
     js_exec(scope, &init_script).unwrap();
-
-    let stdin = io::stdin();
-    print!("> ");
-    io::stdout().flush()?;
 
     for line in stdin.lock().lines() {
         scope.reset();
@@ -74,9 +84,20 @@ async fn main() -> anyhow::Result<()> {
         let apeiro_scope: serde_json::Value = serde_v8::ramson_from_v8(scope, apeiro_scope)?;
         println!("apeiro_scope: {:?}", apeiro_scope);
         println!("");
+        persist_apeiro_scope = Some(apeiro_scope);
 
         print!("> ");
         io::stdout().flush()?;
     }
+
+    if let Some(persist_apeiro_scope) = persist_apeiro_scope {
+        let x = persist_apeiro_scope.to_string();
+        println!("{}", x);
+
+        // write x to file "scope.json"
+        let mut file = std::fs::File::create("scope.json")?;
+        file.write_all(x.as_bytes())?;
+    }
+
     Ok(())
 }
